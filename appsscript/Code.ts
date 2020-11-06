de20463f05dd13a4a6dc189bd9f9478190320c6f
Tim Sampson <timsampson@gmail.com>
2020-11-06 00:45:24 +0000

    update folder structure

--- appsscript/Code.ts
+++
@@ -1,8 +0,0 @@
-
-function doGet(e: any) {
-    return HtmlService.createTemplateFromFile("index").evaluate();
-}
-
-function include(filename: string) {
-    return HtmlService.createHtmlOutputFromFile(filename).getContent();
-}
